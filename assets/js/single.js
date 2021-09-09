18506c08d2f5e72a5d62ae79afa00241d9983623
Derick Hooley <dghooley@gmail.com>
2021-09-09 17:21:12 -0500

    htmlRedirect

--- assets/js/single.js
+++ assets/js/single.js
@@ -4,47 +4,53 @@ var limitWarningEl = document.querySelector("#limit-warning");
 var repoNameEl = document.querySelector("#repo-name");
 
 var getRepoName = function() {
-    var queryString = document.location.search; 
+    // grab repo name from url query string
+    var queryString = document.location.search;
     var repoName = queryString.split("=")[1];
-    getRepoIssues(repoName);
-    repoNameEl.textContent = repoName;
-}
 
+    if (repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
 
-
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+};
 
 var getRepoIssues = function(repo) {
   // format the github api url
-  var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
   // make a get request to url
-  fetch(apiUrl).then(function(response) {
+    fetch(apiUrl).then(function(response) {
     // request was successful
     if (response.ok) {
-      response.json().then(function(data) {
+        response.json().then(function(data) {
         displayIssues(data);
 
         // check if api has paginated issues
         if (response.headers.get("Link")) {
-          displayWarning(repo);
+            displayWarning(repo);
         }
-      });
+    });
     }
     else {
-      console.log(response);
-      alert("There was a problem with your request!");
+        // if not successful, redirect to homepage
+        document.location.replace("./index.html");
     }
-  });
+    });
 };
 
 var displayIssues = function(issues) {
-  if (issues.length === 0) {
+    if (issues.length === 0) {
     issueContainerEl.textContent = "This repo has no open issues!";
     return;
-  }
+    }
 
   // loop over given issues
-  for (var i = 0; i < issues.length; i++) {
+    for (var i = 0; i < issues.length; i++) {
     // create a link element to take users to the issue on github
     var issueEl = document.createElement("a");
     issueEl.classList = "list-item flex-row justify-space-between align-center";
